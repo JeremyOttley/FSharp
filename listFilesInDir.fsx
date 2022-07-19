6e951f69836503600cced401e75c0df4af59faae
Jeremy Ottley <Gazaunga@gmail.com>
2022-07-19 10:56:52 -0400

    Update listFilesInDir.fsx

--- listFilesInDir.fsx
+++ listFilesInDir.fsx
@@ -1,5 +1,7 @@
 open System.IO
 
-Directory.GetFiles("C:\\Users\\jottley\\FSharp\\", "*.fsx")
-  |> Array.map Path.GetFileName
-  |> Array.iter (printfn "%s")
+// find all files in dir but only keep the ones with the extension ".xml"  
+let xmlArray (path:string) =
+  Directory.GetFiles(path, "*")
+    |> Array.map Path.GetFileName
+    |> Array.filter (fun f -> Path.GetExtension(f) = ".xml")
