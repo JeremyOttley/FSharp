26c04e7c32000801f55efc118c65ad9ea6931e9b
Jeremy Ottley <Gazaunga@gmail.com>
2022-07-19 11:55:57 -0400

    Update listFilesInDir.fsx

--- listFilesInDir.fsx
+++ listFilesInDir.fsx
@@ -5,3 +5,10 @@ let xmlArray (path:string) =
   Directory.GetFiles(path, "*")
     |> Array.map Path.GetFileName
     |> Array.filter (fun f -> Path.GetExtension(f) = ".xml")
+
+// get user input from the commandline
+printfn "Give me a path: "
+let path = System.Console.ReadLine()
+
+// print resulting array
+printfn "%A" (xmlArray(path))
