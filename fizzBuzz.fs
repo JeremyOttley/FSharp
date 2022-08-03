9ed013b6cf8f31c25f6d1d44c28de9829fa5fd0c
Jeremy Ottley <Gazaunga@gmail.com>
2022-08-03 18:32:18 -0400

    Update fizzBuzz.fs

--- fizzBuzz.fs
+++ fizzBuzz.fs
@@ -1,17 +1,18 @@
-module FizzBuzz_Match =
+module FizzBuzz_UsingFactorRules =
 
-    let fizzBuzz i =
-        match i with
-        | _ when i % 15 = 0 ->
-            printf "FizzBuzz"
-        | _ when i % 3 = 0 ->
-            printf "Fizz"
-        | _ when i % 5 = 0 ->
-            printf "Buzz"
-        | _ ->
+    let fizzBuzz rules i  =
+        let mutable printed = false
+
+        for factor,label in rules do
+            if i % factor = 0 then
+                printed <- true
+                printf "%s" label
+
+        if not printed then
             printf "%i" i
 
         printf "; "
 
     // do the fizzbuzz
-    [1..100] |> List.iter fizzBuzz
+    let rules = [ (3,"Fizz"); (5,"Buzz") ]
+    [1..100] |> List.iter (fizzBuzz rules)
