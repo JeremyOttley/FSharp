dbc0425f9b8aa9ac6b62a608e7419843f5457013
Jeremy Ottley <Gazaunga@gmail.com>
2022-08-08 19:16:10 -0400

    Update renamer.fsx

--- renamer.fsx
+++ renamer.fsx
@@ -1,20 +1,24 @@
-open System
 open System.IO
 
-let currentDir = Directory.GetFiles(path, "*")
-  |> Array.map Path.GetFileName
-  |> Array.filter (fun f -> Path.GetExtension(f) = ext)
+let path = @"C:\Users\jottley\rename-test"
 
-let getImages (path:string ext:string) = 
-  Directory.GetFiles(path, "*")
-    |> Array.map Path.GetFileName
-    |> Array.filter (fun f -> Path.GetExtension(f) = ext)
+let isImage (file: string) =
+  let extension = Path.GetExtension(file)
+  match extension with
+  | ".jpg" | ".png" | ".bmp" | ".jpeg" -> true
+  | _-> false
 
-let jpgs = Directory.GetFiles("*", "*.jpg")
-let bmps =
-let pngs =
-let jpegs =
+let images = 
+    Directory.GetFiles(path, "*") 
+        |> Array.map Path.GetFileName 
+        |> Array.filter (fun f -> isImage(f))
 
-let randomString =
+let shuffle (coll:seq<char>) =
+  let random = System.Random()
+  coll |> Seq.sortBy (fun _ -> random.Next(1, 52))
 
-//let filename = Path.ChangeExtension(path, "txt")
+let ranStr n =
+    let rand = System.Random()
+    let chars = Array.concat([[|'a' .. 'z'|];[|'A' .. 'Z'|];[|'0' .. '9'|]])
+    let sz = Array.length chars in
+    String(Array.init n (fun _ -> chars.[rand.Next sz]))
