77ef0228af62425290e58436bc5ccc96f0a7b8ec
Jeremy Ottley <Gazaunga@gmail.com>
2022-08-11 13:38:32 -0400

    Create Acronym.fsx

---
+++ Acronym.fsx
@@ -0,0 +1,6 @@
+module Acronym
+
+    let abbreviate (phrase:string) =
+        phrase.ToLower()
+        |> System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase
+        |> String.filter System.Char.IsUpper
