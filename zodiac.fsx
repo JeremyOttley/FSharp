20bdecab9ee946d842ac3a8c509e09e43afa2533
Jeremy Ottley <Gazaunga@gmail.com>
2022-08-14 18:36:47 -0400

    Update zodiac.fsx

--- zodiac.fsx
+++ zodiac.fsx
@@ -1,1 +1,10 @@
+open System
 
+let getZodiac =
+    query
+    let sign = Console.ReadLine()
+    match sign with
+    | "aries" | "taurus" | "gemini" | "cancer" 
+    | "leo" | "virgo"| "libra" | "scorpio" | "sagittarius" 
+    | "capricorn" | "aquarius" | "pisces" -> getFortune(sign)
+    | _ -> failwith "Not a traditional Zodiac sign"
