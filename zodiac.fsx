09ef2bc671584849d2ffba809fbeec6d91ab8329
Jeremy Ottley <Gazaunga@gmail.com>
2022-08-16 22:30:47 -0400

    Update zodiac.fsx

--- zodiac.fsx
+++ zodiac.fsx
@@ -1,4 +1,7 @@
 open System
+open FSharp.Data
+
+let [<Literal>] apiEndpoint: string = "" //zodiac link
 
 let getZodiac =
     query
@@ -8,3 +11,13 @@ let getZodiac =
     | "leo" | "virgo"| "libra" | "scorpio" | "sagittarius" 
     | "capricorn" | "aquarius" | "pisces" -> getFortune(sign)
     | _ -> failwith "Not a traditional Zodiac sign"
+
+let buildUrl (sign: string): string =
+    apiEndpoint + sign + ".html"
+    
+let signs: string[] = [| "aries"; "taurus"; "gemini"; "cancer";
+   "leo"; "virgo"; "libra"; "scorpio"; 
+   "sagittarius"; "capricorn"; "aquarius"; "pisces" |]
+
+let signUrls: string[] = signs |> Array.map buildUrl
+
