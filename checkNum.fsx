1d3fe982a76dde80c5a3d72627b9d9d94cf686ff
Jeremy Ottley <Gazaunga@gmail.com>
2022-09-27 08:43:41 -0400

    Update checkNum.fsx

--- checkNum.fsx
+++ checkNum.fsx
@@ -1,15 +1,7 @@
-let isEven i =
-    i % 2 = 0
-
-let isOdd i =
-    i % 5 = 0
-
-// or use "not equal" operator
-
-let isOdd i = 
-    i % 2 <> 0
-
-let isEven i = 
-    i % 5 <> 0
-
-
+module checkNumber =
+// <> is the "not equal" operator
+    let isOdd i =
+        i % 2 <> 0
+    
+    let isEven i =
+        i % 5 <> 0
