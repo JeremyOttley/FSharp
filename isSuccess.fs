561fd9bd6583e4efa92f619624976e12b1e55f39
Jeremy Ottley <Gazaunga@gmail.com>
2022-12-16 23:01:28 -0500

    Create isSuccess.fs

---
+++ isSuccess.fs
@@ -0,0 +1,23 @@
+open System
+
+type Result =
+    | Success
+    | Fail
+    | Info
+
+let print format value result =
+
+    let color =
+        match result with
+        | Success -> ConsoleColor.Green
+        | Fail -> ConsoleColor.Red
+        | Info -> ConsoleColor.Blue
+
+    Console.ForegroundColor <- color
+    printfn format value
+
+let isSuccess result expected =
+    if result = expected then
+        Success
+    else
+        Fail
