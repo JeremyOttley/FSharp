df173b0588ea30d0fe743ac7f575ca79c2995f4c
Jeremy Ottley <Gazaunga@gmail.com>
2022-12-18 01:46:53 -0500

    Create getUrl.fsx

---
+++ getUrl.fsx
@@ -0,0 +1,22 @@
+open System.Net.Http
+
+let getAsync (client: System.Net.HttpClient) (url:string) = 
+    async {
+        let! response = client.GetAsync(url) |> Async.AwaitTask
+        response.EnsureSuccessStatusCode () |> ignore
+        let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        return content
+    }
+
+// or deprecated version:
+
+open System.IO
+
+let getHttp (url: string) =
+    let req = System.Net.WebRequest.Create(url)
+    let resp = req.GetResponse()
+    let stream = resp.GetResponseStream()
+    let reader = new StreamReader(stream)
+    let html = reader.ReadToEnd()
+    resp.Close()
+    html
