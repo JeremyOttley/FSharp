3c2af1da6f325a3c849f0e0fd0adb3632125c51f
Jeremy Ottley <Gazaunga@gmail.com>
2022-12-26 22:36:54 -0500

    Update readFile.fsx

--- readFile.fsx
+++ readFile.fsx
@@ -1,23 +1,5 @@
-open System.IO
-
-// .NET version
-val readLines: filePath: string -> seq<string>
-let readLines (filePath: string) =
-    seq {
-        use sr = new StreamReader(filePath)
-
-        while not sr.EndOfStream do
-            yield sr.ReadLine()
-    }
-
-// Optimized version
-val readLines: filePath: string -> Collections.Generic.IEnumerable<string>
-let readLines filePath = System.IO.File.ReadLines(filePath)
-
-// Safe version
-val readFile: filename: string -> string option
-let readFile (filename: string) : string option =
-    if File.Exists filename then
-        Some(File.ReadAllText filename)
-    else
-        None
+let readFile (filename: string): string option =
+	try
+		Some(System.IO.File.ReadAllText filename)
+	with
+	| :? System.IO.FileNotFoundException -> printfn "File not found"; None;;
