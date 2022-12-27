5b6c17970872506703d455116500c935f61d2e7f
Jeremy Ottley <Gazaunga@gmail.com>
2022-12-26 23:03:00 -0500

    Update fibonacci.fsx

--- fibonacci.fsx
+++ fibonacci.fsx
@@ -1,4 +1,4 @@
-let rec fibonacci (n: int): float =
+let rec fibonacci (n: int): int =
     match n with
-    | n when (n < 2) -> (float n)
+    | n when (n < 2) -> n
     | _ -> fibonacci(n - 1) + fibonacci(n - 2)
