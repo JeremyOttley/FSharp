a71b49a2e669ee2d24fa5fa04b9dabb6e8f2ef2f
Jeremy Ottley <Gazaunga@gmail.com>
2023-01-01 20:26:11 -0500

    Create partial.fs

---
+++ partial.fs
@@ -0,0 +1,12 @@
+let (|DivisibleBy|_|) by n =
+    if n % by = 0 then 
+        Some DivisibleBy 
+    else None
+
+let fizzBuzz = function
+    | DivisibleBy 3 & DivisibleBy 5 -> "FizzBuzz"
+    | DivisibleBy 3 -> "Fizz"
+    | DivisibleBy 5 -> "Buzz"
+    | i -> string i
+    
+    ;;List.map fizzBuzz [1..20]
