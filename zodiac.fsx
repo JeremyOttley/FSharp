7d1ab21063d563dfd6b264cf39f0083bb9598b3a
Jeremy Ottley <Gazaunga@gmail.com>
2023-02-21 10:17:12 -0500

    Update zodiac.fsx

--- zodiac.fsx
+++ zodiac.fsx
@@ -1,30 +1,51 @@
 open System
-open FSharp.Data
+//open Argu
+// open FSharp.Data
 
 let [<Literal>] apiEndpoint: string = "https://www.astrology.com/horoscope/daily/"
 
-let Zodiac = HtmlProvider<url>
+// let Zodiac = HtmlProvider<url>
 
-let doc = HtmlDocument.Load(url)
-doc.Descendants
+// let doc = HtmlDocument.Load(url)
+// doc.Descendants
 
-let getZodiac: obj  =
-    query
-    let sign = Console.ReadLine()
-    match sign with
-    | "aries" | "taurus" | "gemini" | "cancer" 
-    | "leo" | "virgo"| "libra" | "scorpio" | "sagittarius" 
-    | "capricorn" | "aquarius" | "pisces" -> getFortune(sign)
-    | _ -> failwith "Not a traditional Zodiac sign"
+// let getZodiac: obj  =
+//     query
+//     let sign = Console.ReadLine()
+//     match sign with
+//     | "aries" | "taurus" | "gemini" | "cancer" 
+//     | "leo" | "virgo"| "libra" | "scorpio" | "sagittarius" 
+//     | "capricorn" | "aquarius" | "pisces" -> getFortune(sign)
+//     | _ -> failwith "Not a traditional Zodiac sign"
+
+// let signUrls: string[] = 
+//     signs |> Array.map buildUrl
 
 let buildUrl (sign: string): string =
-    apiEndpoint + sign + ".html"
-    
-let signs: string[] = 
+    apiEndpoint + sign.ToLower() + ".html"
+   
+let signs: string[] =
     [| "aries"; "taurus"; "gemini"; "cancer";
-    "leo"; "virgo"; "libra"; "scorpio"; 
+    "leo"; "virgo"; "libra"; "scorpio";
     "sagittarius"; "capricorn"; "aquarius"; "pisces" |]
 
-let signUrls: string[] = 
-    signs |> Array.map buildUrl
+// let signUrls: string[] =
+//     signs |> Array.map buildUrl
 
+let getSign sign =
+match sign with
+  | "Aries" | "aries" -> buildUrl sign
+  | "Taurus" | "taurus" -> buildUrl sign
+  | "Gemini" | "gemini" -> buildUrl sign
+  | "Cancer" | "cancer" -> buildUrl sign
+  | "Leo" | "leo" -> buildUrl sign
+  | "Virgo" | "virgo" -> buildUrl sign
+  | "Libra" | "libra" -> buildUrl sign
+  | "Scorpio" | "scorpio" -> buildUrl sign
+  | "Sagittarius" | "sagittarius" -> buildUrl sign
+  | "Capricorn" | "capricorn" -> buildUrl sign
+  | "Aquarius" | "aquarius" -> buildUrl sign
+  | "Pisces" | "pisces" -> buildUrl sign
+  | _ -> failwith "Not a traditional Zodiac sign"
+
+//[<EntryPoint>]
