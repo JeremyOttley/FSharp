1510778c5bb96aff1ea75680ede9267343eddbae
Jeremy Ottley <Gazaunga@gmail.com>
2023-03-07 17:47:40 -0500

    Create getCapture.fsx

---
+++ getCapture.fsx
@@ -0,0 +1,12 @@
+// simple
+let getCapture s p =
+  let m = Regex.Match(f, "\[(\w+)\]")
+  [for x in m.Groups -> x.Value].Item(1)
+
+// option
+let getCapture s p =
+  let m = Regex.Match(f, "\[(\w+)\]")
+  [for g in (m.Groups |> Seq.skip 1) -> 
+	if g.Success then Some g.Value else None].Head
+  
+// getCapture "20D4EB1 [GA4D84F4].mp4" "\[(\w+)\]" //=> "GA4D84F4"
