2aa1ff4d1174941a2a7b9563692ba9a51e8276b9
Jeremy Ottley <Gazaunga@gmail.com>
2023-03-08 21:37:28 -0500

    Update getCapture.fsx

--- getCapture.fsx
+++ getCapture.fsx
@@ -3,12 +3,12 @@ open System.Text.RegularExpressions
 // simple
 let getCapture s p =
   let m = Regex.Match(s, p)
-  [for x in m.Groups -> x.Value].Item(1)
+  [for x in (m.Groups |> Seq.skip 1) -> x.Value]
 
 // option
 let getCapture s p =
   let m = Regex.Match(s, p)
   [for g in (m.Groups |> Seq.skip 1) -> 
-	if g.Success then Some g.Value else None].Head
+	if g.Success then Some g.Value else None]
   
-// getCapture "20D4EB1 [GA4D84F4].mp4" "\[(\w+)\]" //=> "GA4D84F4"
+// getCapture "20D4EB1 [GA4D84F4].mp4" "\[(\w+)\]" //=> ["GA4D84F4"]
