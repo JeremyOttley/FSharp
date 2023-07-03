fef5d4f0e977a066f00e9e565c2f2a02fa56abfe
Jeremy Ottley <Gazaunga@gmail.com>
2023-07-03 09:16:05 -0400

    Update renamer.fsx

--- renamer.fsx
+++ renamer.fsx
@@ -1,17 +1,12 @@
+open System
 open System.IO
 
-let path = @"C:\Users\jottley\rename-test"
+let dir = Directory.GetCurrentDirectory()
 
 let isImage (file: string) =
     match Path.GetExtension(file) with
     | ".jpg" | ".png" | ".bmp" | ".jpeg" -> true
     | _-> false
-
-
-let images = 
-    Directory.GetFiles(path, "*") 
-        |> Array.map Path.GetFileName 
-        |> Array.filter (fun f -> isImage(f))
 
 let shuffle (coll:seq<char>) =
     let random = System.Random()
@@ -22,3 +17,8 @@ let ranStr n =
     let chars = Array.concat([[|'a' .. 'z'|];[|'A' .. 'Z'|];[|'0' .. '9'|]])
     let sz = Array.length chars in
     String(Array.init n (fun _ -> chars.[rand.Next sz]))
+
+Directory.GetFiles(dir)
+  |> Seq.filter (fun x -> isImage x)
+  |> Seq.map (fun x -> (x, x.Replace(x, ((ranStr 16) + Path.GetExtension(x)))))
+  |> Seq.iter (fun (x,y) -> File.Move(x, y))
