287e9e319cc54588ce893f641bc018b285302b57
Jeremy Ottley <Gazaunga@gmail.com>
2024-05-04 20:09:08 -0400

    Update getCapture.fsx

--- getCapture.fsx
+++ getCapture.fsx
@@ -10,10 +10,10 @@ let getCapture s p =
 
 // option
 let getCapture s p =
-  let m = Regex.Match(s, p)
-  let result = 
-	[for g in (m.Groups |> Seq.skip 1) -> 
-		if g.Success then Some g.Value else None]
-  result.Head.Value
+    let m = Regex.Match(s, p)
+    let result =
+        [ for g in (m.Groups |> Seq.skip 1) -> if g.Success then Some g.Value else None ]
+    result.Head.Value
+
   
 // getCapture "20D4EB1 [GA4D84F4].mp4" "\[(\w+)\]" //=> "GA4D84F4"
