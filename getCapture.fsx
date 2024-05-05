28244934f422c8e81e7a3b0cf57f4f546ee06dd5
Jeremy Ottley <Gazaunga@gmail.com>
2024-05-04 20:08:24 -0400

    Update getCapture.fsx

--- getCapture.fsx
+++ getCapture.fsx
@@ -2,9 +2,11 @@ open System.Text.RegularExpressions
 
 // simple
 let getCapture s p =
-  let m = Regex.Match(s, p)
-  let result = [for x in (m.Groups |> Seq.skip 1) -> x.Value]
-  result.Head
+    let m = Regex.Match(s, p)
+    let result =
+        [ for g in (m.Groups |> Seq.skip 1) -> if g.Success then Some g.Value else None ]
+    result.Head.Value
+
 
 // option
 let getCapture s p =
